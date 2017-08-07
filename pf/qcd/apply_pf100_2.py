f50b5e8785852fd8bbe3760326ef12a1402a431e
Sid Narayanan <sidn@mit.edu>
2017-08-07 15:50:09 -0400

    cleanup

--- pf/qcd/apply_pf100_2.py
+++ pf/qcd/apply_pf100_2.py
@@ -45,7 +45,6 @@ f_vars = {
   'tau32' : (lambda x : x['singletons'][:,obj.singletons['tau32']], np.arange(0,1.2,0.01)),
   'msd'   : (lambda x : x['singletons'][:,obj.singletons['msd']], np.arange(0.,400.,10.)),
   'pt'    : (lambda x : x['singletons'][:,obj.singletons['pt']], np.arange(250.,1000.,50.)),
-  'dnn'   : (predict, np.arange(0,1.2,0.01)),
 }
 
 OUTPUT = '/home/snarayan/public_html/figs/testplots/qcd13/'
@@ -54,39 +53,45 @@ system('mkdir -p '+OUTPUT)
 p = utils.Plotter()
 r = utils.Roccer()
 
-# now mask the mass
-# def mask(data):
-#     lower = data['singletons'][:,obj.singletons['msd']] > 110
-#     higher = data['singletons'][:,obj.singletons['msd']] < 210
-#     pt = data['singletons'][:,obj.singletons['pt']] > 400 
-#     return np.logical_and(pt, np.logical_and(lower,higher))
-# 
-# hists_top = top_4.draw(components=['singletons', 'inclusive'],
-#                        f_vars=f_vars, f_mask=mask, n_batches=n_batches,
-#                        partition=partition)
-# hists_qcd = qcd_0.draw(components=['singletons', 'inclusive'],
-#                        f_vars=f_vars, f_mask=mask, n_batches=n_batches,
-#                        partition=partition)
-# 
-# for k in hists_top:
-#     htop = hists_top[k]
-#     hqcd = hists_qcd[k]
-#     htop.scale() 
-#     hqcd.scale()
-#     p.clear()
-#     p.add_hist(htop, '3-prong', 'r')
-#     p.add_hist(hqcd, '1-prong', 'k')
-#     p.plot({'output':OUTPUT+'masked_'+k})
-# 
-# r.clear()
-# r.add_vars(hists_top,
-#            hists_qcd,
-#            {'tau32':r'$\tau_{32}$', 'dnn':'DNN', 'msd':r'$m_{SD}$'},
-#            {'tau32':'k', 'dnn':'r', 'msd':'b'})
-# r.plot({'output':OUTPUT+'masked_roc'})
+# now mask the DNN
+def mask(data, cut):
+    dnn = predict(data)
+    return dnn > cut
+
+hists_t = top_3.draw(components=['singletons', 'inclusive'],
+                     f_vars=f_vars, n_batches=n_batches, f_mask = lambda x : mask(x, 0.4))
+hists_3 = qcd_3.draw(components=['singletons', 'inclusive'],
+                     f_vars=f_vars, n_batches=n_batches, f_mask = lambda x : mask(x, 0.4))
+hists_2 = qcd_2.draw(components=['singletons', 'inclusive'],
+                     f_vars=f_vars, n_batches=n_batches, f_mask = lambda x : mask(x, 0.4))
+hists_1 = qcd_1.draw(components=['singletons', 'inclusive'],
+                     f_vars=f_vars, n_batches=n_batches, f_mask = lambda x : mask(x, 0.4))
+
+for k in hists_1:
+    ht = hists_t[k]
+    h1 = hists_1[k]
+    h2 = hists_2[k]
+    h3 = hists_3[k]
+    ht.scale() 
+    h1.scale() 
+    h2.scale() 
+    h3.scale() 
+    p.clear()
+    p.add_hist(ht, '3-prong Top', 'b')
+    p.add_hist(h3, '3-prong QCD', 'r')
+    p.add_hist(h2, '2-prong QCD', 'g')
+    p.add_hist(h1, '1-prong QCD', 'k')
+    p.plot({'output':OUTPUT+'unmasked_'+k})
 
 
-# unmasked first
+# unmasked now
+f_vars = {
+  'tau32' : (lambda x : x['singletons'][:,obj.singletons['tau32']], np.arange(0,1.2,0.01)),
+  'msd'   : (lambda x : x['singletons'][:,obj.singletons['msd']], np.arange(0.,400.,10.)),
+  'pt'    : (lambda x : x['singletons'][:,obj.singletons['pt']], np.arange(250.,1000.,50.)),
+  'dnn'   : (predict, np.arange(0,1.2,0.01)),
+}
+
 hists_t = top_3.draw(components=['singletons', 'inclusive'],
                      f_vars=f_vars, n_batches=n_batches)
 hists_3 = qcd_3.draw(components=['singletons', 'inclusive'],
