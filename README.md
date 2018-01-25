b26cb673063c229f0c4147682106792c6c3ec23b
Sid Narayanan <sidn@mit.edu>
2018-01-25 14:13:04 -0500

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SubtLeNet
 SUBsTructure-LEarning NETworks
+
+## Installation
+
+Either have your `PYTHONPATH` contain `SubtLeNet/python` or do:
+```bash
+cd SubtLeNet/python
+pip install . [--user]
+```
+to make the package available system-wide.
--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@
 
 # global
 export SUBTLENET="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=${PYTHONPATH}:${SUBTLENET}/python/
+export PYTHONPATH=${PYTHONPATH}:${SUBTLENET}/python/ # use this for dev purposes
 
 # config
-#export SUBTLENET_DATA="/fastscratch/snarayan/pandaarrays/v1/"
-#mkdir -p $SUBTLENET_DATA
 export SUBTLENET_FIGSDIR="/home/snarayan/public_html/figs/deep/v1"
 mkdir -p $SUBTLENET_FIGSDIR
 
