f1a6220995887ce080b1bead10ca196eeedc2ad5
Sid Narayanan <sidn@mit.edu>
2018-01-29 13:28:48 -0500

    v0.1.1

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='subtlenet',
-      version='0.1',
+      version='0.1.1',
       description='substructure-learning networks',
       url='https://github.com/sidnarayanan/SubtLeNet',
       author='Sid Narayanan',
