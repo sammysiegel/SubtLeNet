function of p4, fix neutral masking

python/subtlenet/backend/smear.py
@@ -24,29 +24,39 @@ def gauss(arr, mu, sigma):
     return arr 
     
 
-# TODO - implement parameters as functions of p4
+class _Callable(object):
+    def __init__(self, f):
+        if isinstance(f, Number):
+            self.f = lambda x : f
+        else:
+            self.f = f
+    def __call__(self, args):
+        return self.f(*args)
+
 class CaloSmear(object):
     def __init__(self, c_mu, c_sigma, n_mu, n_sigma):
-        self.c_mu = c_mu
-        self.c_sigma = c_sigma
-        self.n_mu = n_mu
-        self.n_sigma = n_sigma
+        self.c_mu = _Callable(c_mu)
+        self.c_sigma = _Callable(c_sigma)
+        self.n_mu = _Callable(n_mu)
+        self.n_sigma = _Callable(n_sigma)
+        self._n_mask = None
 
     @staticmethod
     def _is_neutral(ptype):
+        # forgive me 
+        ptype = ((ptype + 2.5958684827557796) * 2.2772611292950851).astype(np.int)
         return np.logical_or(ptype == 0,                 # calo object
                              np.logical_or(ptype == 3,   # photon
                                            ptype == 4))  # neutral hadron
                 
     def _mu(self, p4, ptype):
-        n = CaloSmear._is_neutral(ptype)
-        return (self.n_mu * n) + (self.c_mu * ~n)
+        return (self.n_mu(p4) * self._n_mask) + (self.c_mu(p4) * ~(self._n_mask))
 
     def _sigma(self, p4, ptype):
-        n = CaloSmear._is_neutral(ptype)
-        return (self.n_sigma * n) + (self.c_sigma * ~n)
+        return (self.n_sigma(p4) * self._n_mask) + (self.c_sigma(p4) * ~(self._n_mask))
 
     def __call__(self, arr):
+        self._n_mask = CaloSmear._is_neutral(arr[:,:,6])
         return gauss(arr, self._mu, self._sigma)
         
 