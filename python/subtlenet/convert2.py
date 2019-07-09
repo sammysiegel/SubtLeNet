ee4313be22749ebdfedde83a9812474f41bd4efe
Rickie Bisnath <rabisnath@uchicago.edu>
2019-07-09 16:06:19 -0500

    added ability to specify a default variable so that if no features are provided, the script will grab the vars with the same shape as the default. also fixed a bunch of little bugs/cleaned up

--- python/subtlenet/convert2.py
+++ python/subtlenet/convert2.py
@@ -27,6 +27,7 @@ with open(args.json) as jsonfile:
     weight = payload['weight']
     cut = payload['cut']
     substructure_vars = payload['substructure_vars']
+    default = payload['default']
 
     y = 0
     for sample in payload['samples']:
@@ -38,7 +39,27 @@ with open(args.json) as jsonfile:
 filenames = [basedir+'/'+sample+'.root' for sample in samples]
 files = [uproot.open(f) for f in filenames]
 trees = [f['Events'] for f in files]
-#print trees[0].keys()
+keys = trees[0].keys()
+#uncomment the line below to see what keys will be accepted for features/defualts in the json
+#print '\n Keys: \n', keys, type(keys)
+
+#if no features are provided, want to grab features w same number of entries as the default
+if features == []:
+    try:
+        default
+    except NameError:
+        print 'no default provided'
+    as_dict = trees[0].arrays(keys)
+    default_shape = as_dict[default].shape
+    #print '\n\ndefault_shape: ', default_shape
+    for k in keys:
+        shape = as_dict[k].shape
+        #print shape
+        if shape == default_shape:
+            #print 'appending^'
+            features.append(k)
+
+#print features
 
 #takes a list of dictionaries and returns a single dictionary
 def merge_dicts(dicts):
@@ -53,12 +74,13 @@ def merge_dicts(dicts):
     return dict(merged)
 
 def get_branches_as_np(branches, mode):
-    multiple_files = len(files) > 1
     dict_ = merge_dicts([tree.arrays(branches) for tree in trees])
-    #print '\n', dict_
+    #print '\n', dict_.values(), type(dict_.values())
     values = np.array(dict_.values())
-    if mode == 'features':
+    if mode == 'features' and values.ndim > 2:
         values = np.swapaxes(values,1,2)
+    elif mode == 'features':
+        pass
     elif mode == 'weight':
         values = values.flatten()
     elif mode != 'substructure':
@@ -82,3 +104,5 @@ save(X, 'x')
 save(Y, 'y')
 save(W, 'w')
 save(substructure, 'ss_vars')
+save(keys, 'all_keys')
+save(features, 'used_keys')
