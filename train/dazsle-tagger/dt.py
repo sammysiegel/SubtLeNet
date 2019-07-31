9ebc0f4efbbbbd714bb36dc5e85cb50fd0242bad
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2019-07-31 12:14:35 -0500

    Simpler training

--- train/dazsle-tagger/dt.py
+++ train/dazsle-tagger/dt.py
@@ -23,19 +23,20 @@ MULTICLASS = False
 REGRESSION = False
 np.random.seed(5)
 
-basedir = '/eos/uscms/store/group/lpcbacon/jkrupa/Jul5/'
-Nqcd = 80000
+basedir = '/eos/uscms/store/user/jkrupa/trainingData/npy/'
+Nqcd = 100000
+Nsig = 100000
 def _make_parent(path):
     os.system('mkdir -p %s'%('/'.join(path.split('/')[:-1])))
 
 class Sample(object):
     def __init__(self, name, base, max_Y):
         self.name = name 
- 
+        self.Yhat = {} 
         if 'QCD' in name:        self.X = np.load('%s/%s_%s.npy'%(base, name, 'x'))[:Nqcd]
-        else:                    self.X = np.load('%s/%s_%s.npy'%(base, name, 'x'))
-        if 'QCD' in name:        self.N2 = np.load('%s/%s_%s.npy'%(base, name, 'ss_vars'))[:Nqcd]
-        else:                    self.N2 = np.load('%s/%s_%s.npy'%(base, name, 'ss_vars'))
+        else:                    self.X = np.load('%s/%s_%s.npy'%(base, name, 'x'))[:Nsig]
+        if 'QCD' in name:        self.SS = np.load('%s/%s_%s.npy'%(base, name, 'ss_vars'))[:Nqcd]
+        else:                    self.SS = np.load('%s/%s_%s.npy'%(base, name, 'ss_vars'))[:Nsig]
 
 
         if REGRESSION:
@@ -54,12 +55,12 @@ class Sample(object):
                         )
               else:
                 self.Y = np_utils.to_categorical(
-                            (np.load('%s/%s_%s.npy'%(base, name, 'y')) > 0).astype(np.int),
+                            (np.load('%s/%s_%s.npy'%(base, name, 'y'))[:Nsig] > 0).astype(np.int),
                             2
                         )
  
         if 'QCD' in name: self.W = np.load('%s/%s_%s.npy'%(base, name, 'w'))[:Nqcd]
-        else:             self.W = np.load('%s/%s_%s.npy'%(base, name, 'w'))
+        else:             self.W = np.load('%s/%s_%s.npy'%(base, name, 'w'))[:Nsig]
         self.idx = np.random.permutation(self.Y.shape[0])
     @property
     def tidx(self):
@@ -75,150 +76,92 @@ class Sample(object):
             return self.idx[:int(VALSPLIT*len(self.idx))]
     def infer(self, model):
         if 'GRU' in model.name: self.X = np.reshape(self.X, (self.X.shape[0], 1, self.X.shape[1])) 
-        self.Yhat = model.predict(self.X)
+        if 'Dense' in model.name: self.X = np.reshape(self.X, (self.X.shape[0],self.X.shape[1]))
+        print model.name, self.X.shape
+        self.Yhat[model.name] = model.predict(self.X)
     def standardize(self, mu, std):
         self.X = (self.X - mu) / std
 
-
-class ClassModelDense(object):
-    def __init__(self, n_inputs, n_hidden, n_targets,samples):
+class ClassModel(object):
+    def __init__(self, n_inputs, h_hidden, n_targets, samples, model):
         self._hidden = 0
-        self.name = 'Dense'
+        self.name = model
         self.n_inputs = n_inputs
         self.n_targets = n_targets if MULTICLASS else 2
         self.n_hidden = n_hidden
-        self.inputs = Input(shape=(int(n_inputs),), name='input')
-        h = self.inputs
-        h = BatchNormalization(momentum=0.6)(h)
-        for _ in xrange(n_hidden-1):
-            h = Dense(int(n_inputs*0.05), activation='relu')(h)
-            h = BatchNormalization()(h)
-        h = Dense(int(n_inputs*0.1), activation='tanh')(h)
-        h = BatchNormalization()(h)
-        if REGRESSION:
-            self.outputs = Dense(1, activation='linear', name='output')(h)
 
-            self.model = Model(inputs=self.inputs, outputs=self.outputs)
-            self.model.compile(optimizer=Adam(),
-                               loss='mse')
-        else:
-            self.outputs = Dense(self.n_targets, activation='softmax', name='output')(h)
-
-            self.model = Model(inputs=self.inputs, outputs=self.outputs)
-            self.model.compile(optimizer=Adam(),
-                               loss='binary_crossentropy')
-        self.model.summary()
         self.tX = np.vstack([s.X[:][s.tidx] for s in samples])
         self.tW = np.concatenate([s.W[s.tidx] for s in samples])
         self.vX = np.vstack([s.X[:][s.vidx] for s in samples])
         self.vW = np.concatenate([s.W[s.vidx] for s in samples])
-
-        if REGRESSION:
-            self.tY = np.concatenate([s.Y[s.tidx] for s in samples])
-            self.vY = np.concatenate([s.Y[s.vidx] for s in samples])
-        else:
-            self.tY = np.vstack([s.Y[s.tidx] for s in samples])
-            self.vY = np.vstack([s.Y[s.vidx] for s in samples])
-            self.tN2 = np.vstack([s.N2[s.tidx] for s in samples])
-            self.vN2 = np.vstack([s.N2[s.vidx] for s in samples])
-        if not REGRESSION:
-            for i in xrange(self.tY.shape[1]):
-                tot = np.sum(self.tW[self.tY[:,i] == 1])
-                self.tW[self.tY[:,i] == 1] *= 100/tot
-                self.vW[self.vY[:,i] == 1] *= 100/tot
-
-    def trainDense(self, samples):
-
-        history = self.model.fit(self.tX, self.tY, sample_weight=self.tW, 
-                                 batch_size=1000, epochs=40, shuffle=True,
-                                 validation_data=(self.vX, self.vY, self.vW))
-        with open('history.log','w') as flog:
-            history = history.history
-            flog.write(','.join(history.keys())+'\n')
-            for l in zip(*history.values()):
-                flog.write(','.join([str(x) for x in l])+'\n')
-    def save_as_keras(self, path):
-        _make_parent(path)
-        self.model.save(path)
-        print 'Saved to',path
-    def save_as_tf(self,path):
-        _make_parent(path)
-        sess = K.get_session()
-        print [l.op.name for l in self.model.inputs],'->',[l.op.name for l in self.model.outputs]
-        graph = graph_util.convert_variables_to_constants(sess,
-                                                          sess.graph.as_graph_def(),
-                                                          [n.op.name for n in self.model.outputs])
-        p0 = '/'.join(path.split('/')[:-1])
-        p1 = path.split('/')[-1]
-        graph_io.write_graph(graph, p0, p1, as_text=False)
-        print 'Saved to',path
-    def predict(self, *args, **kwargs):
-        return self.model.predict(*args, **kwargs)
-    def load_model(self, path):
-        self.model = load_model(path)
-
-class ClassModelGRU(object):
-    def __init__(self, n_inputs, n_hidden, n_targets,samples):
-        self.tX = np.vstack([s.X[:][s.tidx] for s in samples])
-        self.tW = np.concatenate([s.W[s.tidx] for s in samples])
-        self.vX = np.vstack([s.X[:][s.vidx] for s in samples])
-        self.vW = np.concatenate([s.W[s.vidx] for s in samples])
-        self.name = 'GRU'
-        print self.tX.shape
-
-        self.tX = np.reshape(self.tX, (self.tX.shape[0], 1, self.tX.shape[1]))
-        self.vX = np.reshape(self.vX, (self.vX.shape[0], 1, self.vX.shape[1]))
-
-        print self.tX.shape
-        self._hidden = 0
-
-        self.n_inputs = n_inputs
-        self.n_targets = n_targets if MULTICLASS else 2
-        self.n_hidden = n_hidden
-        self.inputs = Input(shape=(1,self.tX.shape[2]), name='input')
-        h = self.inputs
-        
-        NPARTS=20
-        CLR=0.01
-        LWR=0.1
-     
-        gru = GRU(n_inputs,activation='relu',recurrent_activation='hard_sigmoid',name='gru_base')(h)
-        dense   = Dense(100, activation='relu')(gru)
-        norm    = BatchNormalization(momentum=0.6, name='dense4_bnorm')  (dense)
-        dense   = Dense(50, activation='relu')(norm)
-        norm    = BatchNormalization(momentum=0.6, name='dense5_bnorm')  (dense)
-        dense   = Dense(20, activation='relu')(norm)
-        dense   = Dense(10, activation='relu')(dense)
-        outputs = Dense(self.n_targets, activation='sigmoid')(norm)
- 
-        self.model = Model(inputs=self.inputs, outputs=outputs)
-
-        self.model.compile(loss='binary_crossentropy', optimizer=Adam(CLR), metrics=['accuracy'])
 
         self.tY = np.vstack([s.Y[s.tidx] for s in samples])
         self.vY = np.vstack([s.Y[s.vidx] for s in samples])
-        self.tN2 = np.vstack([s.N2[s.tidx] for s in samples])
-        self.vN2 = np.vstack([s.N2[s.vidx] for s in samples])
-        if not REGRESSION:
-            for i in xrange(self.tY.shape[1]):
-                tot = np.sum(self.tW[self.tY[:,i] == 1])
-                self.tW[self.tY[:,i] == 1] *= 100/tot
-                self.vW[self.vY[:,i] == 1] *= 100/tot
+        self.tSS = np.vstack([s.SS[s.tidx] for s in samples])
+        self.vSS = np.vstack([s.SS[s.vidx] for s in samples])
 
-    def trainGRU(self, samples):
+        for i in xrange(self.tY.shape[1]):
+          tot = np.sum(self.tW[self.tY[:,i] == 1])
+          self.tW[self.tY[:,i] == 1] *= 100/tot
+          self.vW[self.vY[:,i] == 1] *= 100/tot
+
+        if 'GRU' in self.name:
+            self.tX = np.reshape(self.tX, (self.tX.shape[0], 1, self.tX.shape[1]))
+            self.vX = np.reshape(self.vX, (self.vX.shape[0], 1, self.vX.shape[1]))
+
+            self.inputs = Input(shape=(1,self.tX.shape[2]), name='input')
+            h = self.inputs
+        
+            NPARTS=20
+            CLR=0.01
+            LWR=0.1
+     
+            gru = GRU(n_inputs,activation='relu',recurrent_activation='hard_sigmoid',name='gru_base')(h)
+            dense   = Dense(100, activation='relu')(gru)
+            norm    = BatchNormalization(momentum=0.6, name='dense4_bnorm')  (dense)
+            dense   = Dense(50, activation='relu')(norm)
+            norm    = BatchNormalization(momentum=0.6, name='dense5_bnorm')  (dense)
+            dense   = Dense(20, activation='relu')(norm)
+            dense   = Dense(10, activation='relu')(dense)
+            outputs = Dense(self.n_targets, activation='sigmoid')(norm)
+            self.model = Model(inputs=self.inputs, outputs=outputs)
+
+            self.model.compile(loss='binary_crossentropy', optimizer=Adam(CLR), metrics=['accuracy'])
+
+        if 'Dense' in self.name:
+            self.inputs = Input(shape=(int(n_inputs),), name='input')
+            h = self.inputs
+            h = BatchNormalization(momentum=0.6)(h)
+            for _ in xrange(n_hidden-1):
+              h = Dense(int(n_inputs*0.05), activation='relu')(h)
+              h = BatchNormalization()(h)
+            h = Dense(int(n_inputs*0.1), activation='tanh')(h)
+            h = BatchNormalization()(h)
+            self.outputs = Dense(self.n_targets, activation='softmax', name='output')(h)
+            self.model = Model(inputs=self.inputs, outputs=self.outputs)
+            self.model.compile(optimizer=Adam(),
+                               loss='binary_crossentropy')
+ 
+        self.model.summary()
+
+
+    def train(self, samples):
 
         history = self.model.fit(self.tX, self.tY, sample_weight=self.tW, 
-                                 batch_size=1000, epochs=10, shuffle=True,
+                                 batch_size=1000000, epochs=1, shuffle=True,
                                  validation_data=(self.vX, self.vY, self.vW))
+
         with open('history.log','w') as flog:
             history = history.history
             flog.write(','.join(history.keys())+'\n')
             for l in zip(*history.values()):
                 flog.write(','.join([str(x) for x in l])+'\n')
+
     def save_as_keras(self, path):
         _make_parent(path)
         self.model.save(path)
         print 'Saved to',path
+
     def save_as_tf(self,path):
         _make_parent(path)
         sess = K.get_session()
@@ -230,10 +173,13 @@ class ClassModelGRU(object):
         p1 = path.split('/')[-1]
         graph_io.write_graph(graph, p0, p1, as_text=False)
         print 'Saved to',path
+
     def predict(self, *args, **kwargs):
         return self.model.predict(*args, **kwargs)
+
     def load_model(self, path):
         self.model = load_model(path)
+
 
 def plot(binning, fn, samples, outpath, xlabel=None, ylabel=None):
     hists = {}
@@ -273,91 +219,101 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--dense', action='store_true')
     parser.add_argument('--gru', action='store_true')
-    parser.add_argument('--model', type=str, default='dense')
+    parser.add_argument('--model',nargs='*',type=str)
     parser.add_argument('--train', action='store_true')
     parser.add_argument('--plot', action='store_true')
     parser.add_argument('--version', type=int, default=0)
     parser.add_argument('--hidden', type=int, default=2)
     args = parser.parse_args()
 
-    basedir = basedir#'/eos/uscms/store/group/lpcbacon/jkrupa/May22_115_2/'
     figsdir = 'plots/%s/'%(args.version)
     modeldir = 'models/evt/v%i/'%(args.version)
 
-    samples = ['VectorDiJet','QCD']
+    SIG = 'BulkGraviton'
+    BKG = 'QCD'
+
+    models = ['Dense','GRU']
+
+    samples = [SIG,BKG]
     samples = [Sample(s, basedir, len(samples)) for s in samples]
     n_inputs = samples[0].X.shape[1]
+    print n_inputs
     print('# sig: ',samples[0].X.shape[0], '#bkg: ',samples[1].X.shape[0])
-    n_hidden = 3
 
     #print 'Standardizing...'
     #mu, std = get_mu_std(samples)
     #[s.standardize(mu, std) for s in samples]
 
-
-    if 'DNN' in args.model:
-        model = ClassModelDense(n_inputs, n_hidden, len(samples),samples)
+    n_hidden = 3
+    if 'Dense' in models:
+        modelDNN = ClassModel(n_inputs, n_hidden, len(samples),samples,'Dense')
         if args.train:
             print 'Training dense...'
-            model.trainDense(samples)
-            model.save_as_keras(modeldir+'/weights_dense.h5')
-            model.save_as_tf(modeldir+'/graph_dense.pb')
+            modelDNN.train(samples)
+            modelDNN.save_as_keras(modeldir+'/weights_dense.h5')
+            modelDNN.save_as_tf(modeldir+'/graph_dense.pb')
         else:
             print 'Loading dense...'
-            model.load_model(modeldir+'weights_dense.h5')
+            modelDNN.load_model(modeldir+'weights_dense.h5')
 
-    if 'GRU' in args.model:
-        model = ClassModelGRU(n_inputs, n_hidden, len(samples),samples)
+        if args.plot:
+            for s in samples:
+              s.infer(modelDNN)
+      
+
+    if 'GRU' in models:
+        modelGRU = ClassModel(n_inputs, n_hidden, len(samples),samples,'GRU')
         if args.train:
             print 'Training gru...'
-            model.trainGRU(samples)
-            model.save_as_keras(modeldir+'/weights_gru.h5')
-            model.save_as_tf(modeldir+'/graph_gru.pb')
+            modelGRU.train(samples)
+            modelGRU.save_as_keras(modeldir+'/weights_gru.h5')
+            modelGRU.save_as_tf(modeldir+'/graph_gru.pb')
         else:
             print 'Loading gru...'
-            model.load_model(modeldir+'weights_gru.h5')
+            modelGRU.load_model(modeldir+'weights_gru.h5')
+        if args.plot:
+            for s in samples:
+              s.infer(modelGRU)
 
     if args.plot:
-        for s in samples:
-            s.infer(model)
 
         samples.reverse()
-        if REGRESSION:
-            plot(np.linspace(60, 160, 20),
-                 lambda s : s.Yhat[s.vidx][:,0],
-                 samples, figsdir+'mass_regressed', xlabel='Regressed mass')
-            plot(np.linspace(60, 160, 20),
-                 lambda s : s.Y[s.vidx],
-                 samples, figsdir+'mass_truth', xlabel='True mass')
-        else:
-            roccer_hists = {}
-            roccer_hists_n = {}
-            roccer_vars_n = {'N2':1}
+        roccer_hists = {}
+        roccer_hists_SS = {}
+        SS_vars = {'N2':1}
+
+        sig_hists = {}
+        bkg_hists = {} 
+
+
+        for idx,num in SS_vars.iteritems():
+                     roccer_hists_SS[idx] = plot(np.linspace(0,1,50),
+                     lambda s: s.SS[s.vidx,0],
+                     samples, figsdir+'%s'%(idx), xlabel='%s'%(idx))
+
+
+        sig_hists['N2'] = roccer_hists_SS['N2'][SIG]    
+        bkg_hists['N2'] = roccer_hists_SS['N2'][BKG]    
+
+        for model in models:
 
             for i in xrange(len(samples) if MULTICLASS else 2):
+                
+                #for model in ['DNN','GRU']
                 roccer_hists = plot(np.linspace(0, 1, 50), 
-                     lambda s, i=i : s.Yhat[s.vidx,i],
-                     samples, figsdir+'class_%i_%s'%(i,args.model), xlabel='Class %i %s'%(i,args.model))
+                       lambda s, i=i : s.Yhat[model][s.vidx,i],
+                       samples, figsdir+'class_%i_%s'%(i,model), xlabel='Class %i %s'%(i,model))
   
+                sig_hists[model] = roccer_hists[SIG]
+                bkg_hists[model] = roccer_hists[BKG] 
 
-                for idx,num in roccer_vars_n.iteritems():
-                     roccer_hists_n[idx] = plot(np.linspace(0,1,50),
-                     lambda s: s.N2[s.vidx,0],
-                     samples, figsdir+'class_%i_%s'%(i,idx), xlabel='Class %i %s'%(i,idx))
+        r1 = utils.Roccer(y_range=range(0,1),axis=[0,1,0,1])
+        r1.clear()
 
-
-            r1 = utils.Roccer(y_range=range(0,1),axis=[0,1,0,1])
-            r1.clear()
-            print roccer_hists
-            sig_hists = {args.model:roccer_hists['VectorDiJet'],
-                'N2':roccer_hists_n['N2']['VectorDiJet']}
-
-            bkg_hists = {args.model:roccer_hists['QCD'],
-                'N2':roccer_hists_n['N2']['QCD']}
-
-            r1.add_vars(sig_hists,           
-                        bkg_hists,
-                        {args.model:args.model,
-                         'N2':'N2'}
-            )
-            r1.plot(figsdir+'class_%s_%sROC'%(str(args.version),args.model))
+        r1.add_vars(sig_hists,           
+                    bkg_hists,
+                    {'Dense':'Dense',
+		     'GRU':'GRU',
+                     'N2':'N2'}
+        )
+        r1.plot(figsdir+'class_%s_ROC'%(str(args.version)))
